6ed17dcb739ad280ce2677e94d3a52e43d0dbb79
jburt22 <jburt@hamilton.edu>
2022-01-27 16:28:14 +0000

    added snow depth and peaks data frame

--- Activity_1.R
+++ Activity_1.R
@@ -1,2 +1,23 @@
 #Activity & HW1
 
+6*6
+6/6
+5^5
+
+myVec <- c(3, 45, 63, 5, 2)
+
+
+myData <- data.frame(col1 = c(4,3),
+                     col2 = c(2,5))
+
+myVec*10
+
+#Prompt 1
+snow_depth <- c(2.5, 3.5, 4.5)
+snow_depth_cm = 2.54*snow_depth
+
+
+Peaks <- data.frame(name = c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak"),
+                    elevation = c(4960, 4926, 4857, 4840),
+                    climb_ascent = c(3570, 4265, 2800, 4178),
+                    trip_length = c(17.8, 17.9, 13.2, 16))
